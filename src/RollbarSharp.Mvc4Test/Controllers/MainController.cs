604720e29e0df034089977554260a02d6f5e8ee7
mroach <mroach@mroach.com>
2013-04-26 16:17:51 -0400

    Dump server vars in MVC test. Add custom data to the exception we raise.

--- src/RollbarSharp.Mvc4Test/Controllers/MainController.cs
+++ src/RollbarSharp.Mvc4Test/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -9,12 +10,19 @@ namespace RollbarSharp.Mvc4Test.Controllers
         public ActionResult Home()
         {
             FormsAuthentication.SetAuthCookie("mroach", true);
+
+            var vars = Request.ServerVariables.AllKeys.Select(k => k + " => " + Request.ServerVariables[k]);
+
+            ViewBag.Vars = string.Join("\n", vars);
+
             return View();
         }
 
         public ActionResult Error()
         {
-            throw new Exception("An error has occurred upon request");
+            var ex = new Exception("An error has occurred upon request");
+            ex.Data["arbitrary_data"] = DateTime.UtcNow;
+            throw ex;
         }
     }
 }
