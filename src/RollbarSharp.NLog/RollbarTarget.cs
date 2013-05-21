454aeb433b951a61bf04ef7466383ebcc60d3368
mroach <mroach@mroach.com>
2013-05-21 10:27:17 -0400

    Add support for configuring Rollbar from the NLog target and using formatting

--- src/RollbarSharp.NLog/RollbarTarget.cs
+++ src/RollbarSharp.NLog/RollbarTarget.cs
@@ -1,32 +1,99 @@
 ﻿using NLog;
 using NLog.Common;
+using NLog.Layouts;
 using NLog.Targets;
 
 namespace RollbarSharp.NLog
 {
+    [Target("Rollbar")]
     public class RollbarTarget : TargetWithLayout
     {
-        protected RollbarClient RollbarClient { get; set; }
+        public string AccessToken { get; set; }
+
+        public string Endpoint { get; set; }
+
+        public Layout Environment { get; set; }
+
+        public Layout Platform { get; set; }
+
+        public Layout Language { get; set; }
+
+        public Layout Framework { get; set; }
+
+
+        public Layout Title { get; set; }
+
 
         public RollbarTarget()
         {
-            RollbarClient = new RollbarClient();
-            RollbarClient.RequestCompleted += RollbarClientRequestCompleted;
+            Title = "${message}";
         }
 
         protected override void Write(LogEventInfo logEvent)
         {
+            var client = CreateClient(logEvent);
             var level = ConvertLogLevel(logEvent.Level);
-            
-            if (logEvent.Exception != null)
-            {
-                RollbarClient.SendException(logEvent.Exception, logEvent.FormattedMessage, level);
-                return;
-            }
-            
-            RollbarClient.SendMessage(logEvent.FormattedMessage, level);
+            var title = Title.Render(logEvent);
+
+            var notice = logEvent.Exception != null
+                             ? client.NoticeBuilder.CreateExceptionNotice(logEvent.Exception)
+                             : client.NoticeBuilder.CreateMessageNotice(logEvent.Message);
+
+            notice.Level = level;
+            notice.Title = title;
+
+            client.Send(notice);
         }
 
+        /// <summary>
+        /// First attemps to create a RollbarClient using config read from
+        /// appSettings. Uses properties of this class as overrides if specified.
+        /// </summary>
+        /// <param name="logEvent"></param>
+        /// <returns></returns>
+        private RollbarClient CreateClient(LogEventInfo logEvent)
+        {
+            var client = new RollbarClient();
+            client.RequestStarting += RollbarClientRequestStarting;
+            client.RequestCompleted += RollbarClientRequestCompleted;
+            
+            if (!string.IsNullOrEmpty(AccessToken))
+                client.Configuration.AccessToken = AccessToken;
+
+            if (!string.IsNullOrEmpty(Endpoint))
+                client.Configuration.Endpoint = Endpoint;
+
+            if (Environment != null)
+                client.Configuration.Environment = Environment.Render(logEvent);
+
+            if (Platform != null)
+                client.Configuration.Platform = Platform.Render(logEvent);
+
+            if (Language != null)
+                client.Configuration.Language = Language.Render(logEvent);
+
+            if (Framework != null)
+                client.Configuration.Framework = Framework.Render(logEvent);
+
+            return client;
+        }
+
+        /// <summary>
+        /// When posting the request to Rollbar, log it to the internal NLog log
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="args"></param>
+        private static void RollbarClientRequestStarting(object source, RequestStartingEventArgs args)
+        {
+            var client = (RollbarClient)source;
+            InternalLogger.Debug("Sending request to {0}: {1}", client.Configuration.Endpoint, args.Payload);
+        }
+
+        /// <summary>
+        /// When receiving a response from Rollbar, log it to the internal NLog log
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="args"></param>
         private static void RollbarClientRequestCompleted(object source, RequestCompletedEventArgs args)
         {
             if (args.Result.IsSuccess)
