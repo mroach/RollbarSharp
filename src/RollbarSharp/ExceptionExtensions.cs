dd25634eff27c82ceddd13c690e9e60176a9e336
mroach <mroach@mroach.com>
2013-11-26 14:21:50 -0500

    Add SendToRollbar extension method to Exception

---
+++ src/RollbarSharp/ExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RollbarSharp.Serialization;
+
+namespace RollbarSharp
+{
+    public static class ExceptionExtensions
+    {
+        public static void SendToRollbar(this Exception ex, string title = null, string level = "error", Action<DataModel> modelAction = null)
+        {
+            if (ex == null)
+                return;
+
+            (new RollbarClient()).SendException(ex, title, level, modelAction);
+        }
+    }
+}
