19b49c7ec824a7a07147feb4f05f26395066ea62
Alberto Monteiro <alberto.monteiro@live.com>
2015-03-28 11:14:59 -0300

    Using NUnit for tests

---
+++ src/RollbarSharp.Tests/RollbarClientTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RollbarSharp.Tests
+{
+    [TestFixture]
+    public class RollbarClientTest
+    {
+        [Test]
+        public void when_serializing_message_notice_result_should_not_be_empty()
+        {
+            var client = new RollbarClient();
+            var notice = client.NoticeBuilder.CreateMessageNotice("Hello");
+
+            notice.Server.Host = "miker";
+            notice.Request.Url = "http://localhost/hej";
+            notice.Request.Method = "GET";
+
+            var serialized = client.Serialize(notice);
+
+            Assert.IsNotNullOrEmpty(serialized);
+        }
+
+        [Test]
+        public void when_serializing_exception_notice_result_should_not_be_empty()
+        {
+            var client = new RollbarClient();
+            Exception exception = new NotImplementedException();
+
+            var notice = client.NoticeBuilder.CreateExceptionNotice(exception);
+            notice.Server.Host = "miker";
+            notice.Request.Url = "http://localhost/hej";
+            notice.Request.Method = "GET";
+            notice.Request.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+            notice.Request.UserIp = "67.90.39.34";
+            notice.Person.Id = "123";
+            notice.Person.Username = Environment.UserName;
+
+            var serialized = client.Serialize(notice);
+
+            Assert.IsNotNullOrEmpty(serialized);
+        }
+    }
+}
---
+++ src/RollbarSharp.Tests/Serialization/ExceptionModelTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using RollbarSharp.Builders;
+
+namespace RollbarSharp.Tests.Serialization
+{
+    [TestFixture]
+    public class ExceptionModelTest
+    {
+        [Test]
+        public void when_creating_from_manually_created_exception_should_have_expected_Class()
+        {
+            var ex = new InvalidOperationException("Invalid operation");
+            var model = ExceptionModelBuilder.CreateFromException(ex);
+
+            Assert.AreEqual(typeof(InvalidOperationException).Name, model.Class);
+        }
+
+        [Test]
+        public void when_creating_from_manually_created_exception_should_have_Message_copied()
+        {
+            var ex = new InvalidOperationException("Invalid operation");
+            var model = ExceptionModelBuilder.CreateFromException(ex);
+
+            Assert.AreEqual("Invalid operation", model.Message);
+        }
+
+        [Test]
+        public void when_creating_from_thrown_exception_should_have_expected_Class()
+        {
+            try
+            {
+                CreateException();
+            }
+            catch (Exception ex)
+            {
+                var model = ExceptionModelBuilder.CreateFromException(ex);
+
+                Assert.AreEqual(typeof(DivideByZeroException).Name, model.Class);
+            }
+        }
+
+        [Test]
+        public void when_creating_from_thrown_exception_should_have_Message()
+        {
+            try
+            {
+                CreateException();
+            }
+            catch (Exception ex)
+            {
+                var model = ExceptionModelBuilder.CreateFromException(ex);
+
+                Assert.IsNotNullOrEmpty(model.Message);
+            }
+        }
+
+        private int CreateException()
+        {
+            var x = 0;
+            var y = 1 / x;
+            return y;
+        }
+    }
+}
