ea875d07f038ac08e92d3dfd6eaed0d195e1ded5
Paul Kohlhoff <pkohlhoff@makemusic.com>
2015-04-13 11:30:48 -0600

    adding tests for task result

--- src/RollbarSharp.Tests/RollbarClientTest.cs
+++ src/RollbarSharp.Tests/RollbarClientTest.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using RollbarSharp.Builders;
 
 namespace RollbarSharp.Tests
 {
@@ -39,6 +44,141 @@ namespace RollbarSharp.Tests
             var serialized = client.Serialize(notice);
 
             Assert.IsNotNullOrEmpty(serialized);
+        }
+
+        [Test]
+        public void when_calling_send_exception_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            Exception exception = new NotImplementedException();
+
+            var task = client.SendException(exception);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_critical_exception_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            Exception exception = new NotImplementedException();
+
+            var task = client.SendCriticalException(exception);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        
+        [Test]
+        public void when_calling_send_error_exception_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            Exception exception = new NotImplementedException();
+
+            var task = client.SendErrorException(exception);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_warning_exception_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            Exception exception = new NotImplementedException();
+
+            var task = client.SendWarningException(exception);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_critical_message_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+
+            var task = client.SendCriticalMessage(message);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_error_message_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+
+            var task = client.SendErrorMessage(message);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_warning_message_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+
+            var task = client.SendWarningMessage(message);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_info_message_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+
+            var task = client.SendInfoMessage(message);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_debug_message_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+
+            var task = client.SendDebugMessage(message);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_message_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+
+            var task = client.SendMessage(message, "info");
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
+        }
+
+        [Test]
+        public void when_calling_send_task_returns_and_can_be_used()
+        {
+            var client = new RollbarClient();
+            var message = "";
+            
+            var notice = new DataModelBuilder().CreateMessageNotice(message, "debug");
+
+            var task = client.Send(notice, null);
+
+            Assert.IsNotNull(task);
+            Assert.DoesNotThrow(() => task.Wait(0));
         }
     }
 }
