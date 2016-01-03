d8f0b1df7f61c547d0cec716b0aa3038cc0cca5c
Beñat Gurmendi <bgurmendi@gmail.com>
2016-01-03 17:35:30 +0100

    Added testcase TraceChainModelBuilder on reflection method.

--- src/RollbarSharp.Tests/Serialization/TraceModelTest.cs
+++ src/RollbarSharp.Tests/Serialization/TraceModelTest.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using NUnit.Framework;
 using RollbarSharp.Builders;
+using System.Reflection;
 
 namespace RollbarSharp.Tests.Serialization
 {
@@ -49,11 +50,28 @@ namespace RollbarSharp.Tests.Serialization
             }
         }
 
+		[Test()]
+		public void when_creating_from_thrown_within_reflection_call_exception_should_have_Exception_model()
+		{
+			var method = this.GetType().GetMethod("MethodWithException");
+			try{
+				method.Invoke(this, new object[]{ null});
+				Assert.Fail("La llamada anterior debe lanzar una excepción");
+			}catch(Exception ex){
+				var model = TraceChainModelBuilder.CreateFromException(ex).FirstOrDefault();
+				Assert.IsNotNull(model.Exception);
+			}
+		}
+
         private int GenerateException(string noparam)
         {
             var x = 0;
             var q = 1 / x;
             return q;
         }
+
+		public void MethodWithException(string noparam){
+			throw new ArgumentException("Test exception");
+		}
     }
 }
