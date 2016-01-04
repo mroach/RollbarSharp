142bde66317d20c9080ca31c7e12a95459a408d1
Alberto Monteiro <alberto.monteiro@live.com>
2016-01-04 10:29:30 -0300

    Fixed Mvc4 Test project

--- src/RollbarSharp.Mvc4Test/Controllers/MainController.cs
+++ src/RollbarSharp.Mvc4Test/Controllers/MainController.cs
@@ -15,7 +15,7 @@ namespace RollbarSharp.Mvc4Test.Controllers
             Session["UserId"] = 102;
             Session["CartAmount"] = 123.45m;
             Session["Username"] = "mroach";
-            Session["CartItems"] = new[] {233, 479073, 2323, 9923};
+            Session["CartItems"] = new[] { 233, 479073, 2323, 9923 };
             Session["Userdata"] = new HashSet<string>();
             Session["Password"] = "y u store password?";
             Session["Secret"] = "A23A854F3DD64CC9BEC6D498A2C097D8";
