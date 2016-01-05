a376fe842abaa8750f8ceed94fc17901cfb6419f
Alberto Monteiro <alberto.monteiro@live.com>
2016-01-05 10:02:33 -0300

    Version 0.3.5.0

--- src/RollbarSharp/Properties/AssemblyInfo.cs
+++ src/RollbarSharp/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RollbarSharp")]
 [assembly: AssemblyDescription("Bindings for the Rollbar (rollbar.com) error reporting system")]
 [assembly: AssemblyProduct("RollbarSharp")]
-[assembly: AssemblyVersion("0.3.4.0")]
+[assembly: AssemblyVersion("0.3.5.0")]
